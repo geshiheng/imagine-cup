af7feed1e2840549b3c6f9af42a59aa712c0c9be
ROIM1998 <zhaobw1998@sina.com>
2023-03-20 18:35:54 +0000

    Add basic tests for OpenAI APIs

---
+++ backend/tests/openaitest.py
@@ -0,0 +1,17 @@
+import openai
+APIKEY = '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'
+openai.api_key = APIKEY
+
+def chatgpt_intent_cls(text):
+    answer = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                    {"role": "system", "content": "You are going to act as an AI assistant that would only classify whether the user want to start their workout based on the user's input. When the user is talking about starting their workout, you should output 'start'. When the user is talking about not starting their workout, you should output 'not start'. When the user is talking about something else irrelevant to their workout or health, you should output 'irrelevant'. Your output should be either 'start', 'not start', or 'irrelevant' with nothing else. Do not generate anything extra for explanations but just start, not start, or irrelevant."},
+                    {"role": "user", "content": text},
+                ]
+            )
+    answer_content = answer['choices'][0]['message']['content']
+    return answer_content
+
+audio_file= open("backend/data/static/media/example.wav", "rb")
+transcript = openai.Audio.transcribe("whisper-1", audio_file)

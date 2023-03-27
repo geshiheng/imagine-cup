b3bee2fd199da97920b13036a9c95aa771710ea2
ROIM1998 <bowen98@uw.edu>
2023-03-27 04:04:49 +0000

    Add azure text-to-speech utils

---
+++ backend/zerogym/utils/azure_utils.py
@@ -0,0 +1,29 @@
+import os
+import azure.cognitiveservices.speech as speechsdk
+
+# This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
+audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+
+# The language of the voice that speaks.
+speech_config.speech_synthesis_voice_name='en-US-JennyNeural'
+
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+
+def speech_to_text(text, style, fn='./output.wav'):
+    ssml = open('data/static/docs/assistant.xml', 'r').read()
+    ssml_text = ssml % (speech_config.speech_synthesis_voice_name, style,text)
+    speech_synthesis_result = speech_synthesizer.speak_ssml_async(ssml_text).get()
+
+    if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+        print("Speech synthesized for text [{}]".format(text))
+        stream = speechsdk.AudioDataStream(speech_synthesis_result)
+        stream.save_to_wav_file(fn)
+    elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = speech_synthesis_result.cancellation_details
+        print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            if cancellation_details.error_details:
+                print("Error details: {}".format(cancellation_details.error_details))
+                print("Did you set the speech resource key and region values?")
+    return speech_synthesis_result.audio_data
